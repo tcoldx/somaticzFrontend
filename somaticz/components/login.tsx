876a1652cf971fc1c8d98c69ccda5331c0a23ae5
tcoldx <t.tredis@gmail.com>
2023-02-28 00:42:45 -0800

    first-commit

---
+++ somaticz/components/login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+const Login = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Login Page</Text>
+    </View>
+  );
+};
+
+export default Login;
